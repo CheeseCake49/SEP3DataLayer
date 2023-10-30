2e23a8a6da86c0a5923a0e076a9292607d83e6be
CheeseCake49 <swampjohan@gmail.com>
2023-10-30 11:25:41 +0100

    Initial Commit

---
+++ src/test/java/sep3datalayer/Sep3DataLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package sep3datalayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class Sep3DataLayerApplicationTests
+{
+
+  @Test void contextLoads()
+  {
+  }
+
+}
